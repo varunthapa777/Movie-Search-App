7867acb44b2e4d5a55739648ab58e29c4bfad177
varunthapa777 <varunthapa1920@gmail.com>
2024-09-19 19:33:07 +0530

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>Navigation</nav>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-red-500 text-5xl">Home Page</h1>
+</template>
